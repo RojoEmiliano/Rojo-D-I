64ef28a3ed132c033e66f8f25aa6055f8deaf545
Elian Rivoira <eli_rivoira@hotmail.com>
2023-08-15 14:20:46 -0300

    min-h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './components/index';
 import Footer from './components/footer/footer';
 import NavBar from './components/navBar/navBar';
+import Epson from './components/epson/epson';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+    <div className='min-h-screen flex flex-col'>
       <NavBar />
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/epson" element={<Epson />} />
       </Routes>
       <Footer />
+    </div>
     </BrowserRouter>
   );
 }

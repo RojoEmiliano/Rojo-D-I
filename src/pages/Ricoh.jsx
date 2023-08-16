7d83874087cf6ca69d7811c0d7761815a1e91078
Emiliano Rojo <rojoemiliano515@gmail.com>
2023-08-16 17:23:21 -0300

    ricoh img

--- src/pages/Ricoh.jsx
+++ src/pages/Ricoh.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { products } from '../assets/data/ricoh'; 
+import SliderComponent from '../components/slider';
 
 
 const Ricoh = () => {
@@ -7,12 +8,16 @@ const Ricoh = () => {
     return (
         <div className='w-full border'>
           <div className='flex flex-wrap justify-evenly'>
-            {products.map(product => (
-              <div key={product.name} className='flex flex-col items-center mt-4 border'>
-                <img src={product.imgs[0]} alt='impresora' />
-                <h2 className='font-medium text-xl'>{product.name}</h2>
-              </div>
-            ))}
+          {products.map(product => (
+          <div key={product.name} className='flex flex-col items-center border mt-20 max-w-[500px]'>
+            <div className='w-full max-h-[338px]'>
+              <SliderComponent slides={product.imgs} resY='338px' resX='500px' />
+            </div>
+            <div className='w-full text-center'>
+              <h2 className='font-medium text-xl'>{product.name}</h2>
+            </div>
+          </div>
+        ))}
           </div>
         </div>
       );

88c23fffb1e4a809403f71105e11a0196e8e65ec
Elian Rivoira <eli_rivoira@hotmail.com>
2023-08-17 18:00:35 -0300

    added route to epson detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Index from './components/index';
 import Footer from './components/footer/footer';
 import NavBar from './components/navBar/navBar';
 import Epson from './pages/epson/epson';
 import './App.css';
 import Ricoh from './pages/ricoh';
-import ItemDetail from './components/navBar/ItemDetail/ItemDetail';
+import ItemDetail from './pages/ItemDetail/ItemDetail';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className='min-h-screen flex flex-col'>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path='/ricoh' element={<Ricoh/>}/>
-        <Route path="/epson" element={<Epson />} />
-        <Route path="/ricoh/detail/:modelCode" element={<ItemDetail/>}/>
-      </Routes>
-      <Footer />
-    </div>
+      <div className='min-h-screen flex flex-col'>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Index />} />
+          <Route path='/ricoh' element={<Ricoh />} />
+          <Route path='/epson' element={<Epson />} />
+          <Route path='/ricoh/detail/:modelCode' element={<ItemDetail />} />
+          <Route path='/epson/detail/:modelCode' element={<ItemDetail />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }

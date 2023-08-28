d624c63c53ed021b94ac5f09a6b843070ced97e7
Elian Rivoira <eli_rivoira@hotmail.com>
2023-08-28 17:20:06 -0300

    fixed theme

--- src/commons/ShowMoreButton/ShowMoreButton.jsx
+++ src/commons/ShowMoreButton/ShowMoreButton.jsx
@@ -4,13 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 const ShowMoreButton = ({ theme, modelCode, brand }) => {
+  console.log(theme);
   return (
     <Link
       to={`/${brand}/detail/${modelCode}`}
       className={`lbMax:!bottom-5 lbMax:!z-10 vermas flex justify-end items-center text-white font-regular absolute rounded-md 
-      sbMax:w-[50%] sbMax:px-3 sb:w-[40%] sb:px-4 h-8 bg-${theme} hover:bg-${theme}Hover`}
+      sbMax:w-[50%] sbMax:px-3 sb:w-[40%] sb:px-4 h-8 ${
+        theme === 'epson' ? 'bg-epson hover:bg-epsonHover' : 'bg-ricoh hover:bg-ricohHover'
+      }`}
     >
-      <div className='w-[70%] flex justify-between items-center'>
+      <div className='w-[65%] flex justify-between items-center'>
         <span>Ver Más</span>
         <FontAwesomeIcon icon={faCaretRight} />
       </div>

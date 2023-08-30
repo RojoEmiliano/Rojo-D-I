ab63d367458133e84298996de58ea9ad7de58f44
Elian Rivoira <eli_rivoira@hotmail.com>
2023-08-30 17:58:47 -0300

    fixed scrolling cache when navigate

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Index from './components/index';
 import Footer from './components/footer/footer';
-import NavBar from './components/navBar/navBar';
+import NavBar from './components/NavBar/NavBar';
 import Epson from './pages/epson/epson';
 import './App.css';
 import Ricoh from './pages/ricoh';
 import ItemDetail from './pages/ItemDetail/ItemDetail';
 import About from './pages/About/About';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
@@ -16,12 +17,54 @@ function App() {
       <div className='min-h-screen flex flex-col'>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='/ricoh' element={<Ricoh />} />
-          <Route path='/epson' element={<Epson />} />
-          <Route path='/ricoh/detail/:modelCode' element={<ItemDetail />} />
-          <Route path='/epson/detail/:modelCode' element={<ItemDetail />} />
-          <Route path='/about' element={<About />} />
+          <Route
+            path='/'
+            element={
+              <ScrollToTop>
+                <Index />
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/ricoh'
+            element={
+              <ScrollToTop>
+                <Ricoh />
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/epson'
+            element={
+              <ScrollToTop>
+                <Epson />
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/ricoh/detail/:modelCode'
+            element={
+              <ScrollToTop>
+                <ItemDetail />
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/epson/detail/:modelCode'
+            element={
+              <ScrollToTop>
+                <ItemDetail />
+              </ScrollToTop>
+            }
+          />
+          <Route
+            path='/about'
+            element={
+              <ScrollToTop>
+                <About />
+              </ScrollToTop>
+            }
+          />
         </Routes>
         <Footer />
       </div>
